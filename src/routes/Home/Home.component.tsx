9334f25a252f88ed159c37d1dee4fa2e60cea347
Dinh Phung Quoc (EIT) <dinh.phungquoc@embedit.com>
2024-09-19 12:44:48 +0700

    first commit

---
+++ src/routes/Home/Home.component.tsx
@@ -0,0 +1,10 @@
+import SingleColumnLayout from '../../layouts/SingleColumnLayout/SingleColumnLayout.component'
+
+const Home = () => {
+  return (
+    <SingleColumnLayout
+    />
+  )
+}
+
+export default Home
